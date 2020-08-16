876a1cadd54d442749df4b82aff6f489709937df
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-08-16 16:23:09 +0530

    Latesh.java modified

--- src/Latesh.java
+++ src/Latesh.java
@@ -1,2 +1,25 @@
 public class Latesh {
+    String latesh;
+    int age;
+
+    public Latesh(String latesh, int age) {
+        this.latesh = latesh;
+        this.age = age;
+    }
+
+    public String getLatesh() {
+        return latesh;
+    }
+
+    public void setLatesh(String latesh) {
+        this.latesh = latesh;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
