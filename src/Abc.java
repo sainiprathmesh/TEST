2f7044edec53da56c47c14d4908fba92d1e8b019
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-08-19 12:55:59 +0530

    Add Xyz

---
+++ src/Abc.java
@@ -0,0 +1,6 @@
+/**
+ * @author hp on 8/19/2020 at 12:28 PM
+ */
+public class Abc {
+
+}
---
+++ src/Xyz.java
@@ -0,0 +1,8 @@
+/**
+ * @author hp on 8/19/2020 at 12:53 PM
+ */
+public class Xyz {
+    public static void main(String[] args) {
+        System.out.println("ABC");
+    }
+}
