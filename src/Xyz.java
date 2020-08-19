4181737d7d2dc72e49cf80b93bad962961658cef
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-08-19 12:59:04 +0530

    Add lines

--- src/Xyz.java
+++ src/Xyz.java
@@ -5,5 +5,7 @@ public class Xyz {
     public static void main(String[] args) {
         System.out.println("ABC");
         System.out.println("ABC");
+        System.out.println("ABC");
+
     }
 }
